c04d6b128fa67164d97cb5c61f59100ee3ffe0f7
1arley <arthuriarley323@gmail.com>
2026-02-01 19:47:43 -0300

    feat: configuração do prisma com postgresql local

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 1. Configuração do ValidationPipe (para os DTOs funcionarem)
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true, // Importante para converter datas automagicamente
+  }));
+
+  // 2. Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('EventPass API')
+    .setDescription('Plataforma de gestão de agendas pessoais')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${await app.getUrl()}/api`);
 }
 bootstrap();
